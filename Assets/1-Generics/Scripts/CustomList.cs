c9146797f53e3e1ab12aae6f711f5d03d31977cf
Ryan Fensom <ryan.fensom3@tafensw.net.au>
2017-07-30 18:58:34 +1000

    Added Clear and Remove functions.

--- Assets/1-Generics/Scripts/CustomList.cs
+++ Assets/1-Generics/Scripts/CustomList.cs
@@ -7,6 +7,7 @@ namespace Generics
     public class CustomList<T>
     {
         public T[] list;
+        private bool itemRemoved;
         public int amount { get; private set; }
         // Default constructor
         public CustomList() { amount = 0; }
@@ -22,6 +23,7 @@ namespace Generics
                 list[index] = value;
             }
         }
+
         public void Add(T item)
         {
             // Create a new array of amount + 1
@@ -41,6 +43,42 @@ namespace Generics
             list = cache;
             // Increment amount
             amount++;
+        }
+
+        public void Remove(T item)
+        {
+            // Create a new array of amount - 1
+            T[] cache = new T[amount - 1];
+            // Set boolean to false
+            itemRemoved = false;
+            // Check if the list has been initialized
+            if (list != null)
+            {
+                // Copy all existing items to new array except removed item
+                for (int i = 0; i < list.Length; i++)
+                {
+                    if (EqualityComparer<T>.Default.Equals(list[i], item))
+                    {
+                        itemRemoved = true;
+                    }
+                    else
+                    {
+                        cache[i] = itemRemoved ? list[i - 1] : list[i];
+                    }
+                }
+            }
+            // Replace old array with new array
+            list = cache;
+            // decrease amount
+            amount--;
+        }
+
+        public void Clear()
+        {
+            // list equals nothing
+            list = new T[0];
+            // amount = 0
+            amount = 0;
         }
     }
 }
