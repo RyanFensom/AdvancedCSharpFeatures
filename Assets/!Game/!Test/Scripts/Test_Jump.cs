92a01576daae5eacb6cf247140bc92c4dfbfadbf
RyanFensom <ryan.fensom3@tafensw.net.au>
2017-11-27 14:28:10 +1100

    Adding test components

---
+++ Assets/!Game/!Test/Scripts/Test_Jump.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Jump : Test
+{
+
+    [Header("Test Parameters")]
+    public float minHeight = 1f;
+
+    private PlayerController player;
+    private float originalY;
+    private float jumpApex;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+        originalY = transform.position.y;
+    }
+
+    protected override void Simulate()
+    {
+        // Simulate Jump
+        player.Jump();
+    }
+
+    protected override void Check()
+    {
+        //Get the current player Y position
+        float playerY = transform.position.y;
+        float height = playerY - originalY;
+        // Is the height over the jumpApex?
+        if (height > jumpApex)
+        {
+            jumpApex = height;
+        }
+        // Is the jump heigher than minheight?
+        if (jumpApex > minHeight)
+        {
+            IntegrationTest.Pass(gameObject);
+        }
+    }
+}
